3f88283c313a104098e66935aeffeafb1524b330
bermiferrer <bermiferrer@a2fa5c27-f921-0410-a72c-bf682d381be0>
2007-03-02 03:42:28 +0000

    Avoiding indexes on primary keys for DB session and DB cache
    git-svn-id: http://svn.akelos.org/trunk@108 a2fa5c27-f921-0410-a72c-bf682d381be0

--- app/installers/framework_installer.php
+++ app/installers/framework_installer.php
@@ -5,20 +5,20 @@ class FrameworkInstaller extends AkInstaller
     function up_1()
     {
         $this->createTable('cache', '
-        id string(65) not null index primary key unique,
+        id string(65) not null primary key unique,
         cache_group string(50) index,
         cache_data binary,
         expire datetime'
         );
                 
         $this->createTable('sessions', '
-        id string(32) not null index primary key,
+        id string(32) not null primary key,
         expire datetime,
         value text'
         );
         
         $this->createTable('var', '
-        id string(50) not null index primary key,
+        id string(50) not null primary key,
         val text'
         );
     }
