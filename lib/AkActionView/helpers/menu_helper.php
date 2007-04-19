bbf4a1b7d1441acb9a0df0e21433e718f3b71406
bermiferrer <bermiferrer@a2fa5c27-f921-0410-a72c-bf682d381be0>
2007-04-19 01:11:38 +0000

    Applying Jerome patch #55 with improvements on the menu_helper and new unit tests
    git-svn-id: http://svn.akelos.org/trunk@209 a2fa5c27-f921-0410-a72c-bf682d381be0

--- lib/AkActionView/helpers/menu_helper.php
+++ lib/AkActionView/helpers/menu_helper.php
@@ -17,37 +17,75 @@
  */
 
 
+/**
+* The Menu Helper makes it easier to create simple menus from controllers actions.
+*/
 class MenuHelper extends AkActionViewHelper
 {
 
-    function menu_for_controllers($menu_options = array())
+    /**
+    * Returns a menu for all or several actions in all or several controllers.
+    *
+    * Let +menu_options+ defaults and this will generate a menu with all actions in all controllers.
+    * Set +menu_options+ to an array with keys as controller name and values as actions names.
+    *
+    * <?php echo $menu_helper->menu_for_controllers(array('advertiser' => array('buy', 'partial_in_template'))); ?>
+    * will generate something like :
+    * <div id="menu">
+    *  <ul>
+    *   <li>
+    *    <h2><a href="/advertiser/">Advertiser</a></h2>
+    *    <ul>
+    *     <li><a href="/advertiser/buy/">Buy</a></li>
+    *     <li><a href="/advertiser/partial_in_template/">Partial in template</a></li>
+    *    </ul>
+    *   </li>
+    *  </ul>
+    * </div>
+    *
+    * +div_menu_id+: the id of the main div (default is "menu")
+    * +current_class+: the class name of the current controller or the current action (default is "current")
+    * +title_tag+: the tag that will contain the controller name link (default is "h2"). If it's empty, it won't be present
+    */
+    function menu_for_controllers($menu_options = array(), $div_menu_id = 'menu', $current_class = 'current', $title_tag = 'h2')
     {
         $menu_options = empty($menu_options) ? $this->_get_default_full_menu() : $menu_options;
         $menu = '';
-        foreach ($menu_options as $controller=>$actions){
+        
+        foreach ($menu_options as $controller => $actions){
             $controller_name = AkInflector::classify($controller);
             $current_controller_name = $this->_controller->getControllerName();
             $current_action_name = $this->_controller->Request->getAction();
             $controller_class_name = $controller_name.'Controller';
             $controller_human_name = AkInflector::humanize($controller);
             $controller_file_name = AkInflector::toControllerFilename($controller);
-
             if(file_exists($controller_file_name)){
                 include_once($controller_file_name);
                 if(class_exists($controller_class_name)){
-                    $menu_header = TagHelper::content_tag('h2',TagHelper::content_tag('a', $controller_human_name, array('href'=>$this->_controller->urlFor(array('controller'=>$controller)))), array('class'=> ($current_controller_name == $controller_name ? 'current' : '')));
+                    $class = $current_controller_name == $controller_name ? array('class' => $current_class) : array();
+                    $href = array('href' => $this->_controller->urlFor(array('controller' => $controller)));
+                    if (empty($title_tag)) {
+                        $_title_tag = 'a';
+                        $content = $controller_human_name;
+                        $options = array_merge($class, $href);
+                    } else {
+                        $content = TagHelper::content_tag('a', $controller_human_name, $href);
+                        $options = $class;
+                        $_title_tag = $title_tag;
+                    }
+                    $menu_header = TagHelper::content_tag($_title_tag, $content, $options);
                     $submenu = '';
                     foreach ((array)$actions as $action){
                         if($action[0] == '_'){
                             continue;
                         }
-                        $submenu .= TagHelper::content_tag('li',TagHelper::content_tag('a', AkInflector::humanize($action), array('href'=>$this->_controller->urlFor(array('controller'=>$controller,'action'=>$action)))), array('class'=> ($current_controller_name == $controller_name && $current_action_name == $action ? 'current' : '')));
+                        $submenu .= TagHelper::content_tag('li', TagHelper::content_tag('a', AkInflector::humanize($action), array('href' => $this->_controller->urlFor(array('controller' => $controller, 'action' => $action)))), $current_controller_name == $controller_name && $current_action_name == $action ? array('class' => $current_class) : array());
                     }
-                    $menu .= !empty($submenu) ? TagHelper::content_tag('ul', TagHelper::content_tag('li', $menu_header.TagHelper::content_tag('ul',$submenu))) : '';
+                    $menu .= !empty($submenu) ? TagHelper::content_tag('ul', TagHelper::content_tag('li', $menu_header.TagHelper::content_tag('ul', $submenu))) : '';
                 }
             }
         }
-        return TagHelper::content_tag('div',$menu, array('id'=>'menu'));
+        return TagHelper::content_tag('div', $menu, array('id' => $div_menu_id));
     }
 
     function _get_default_full_menu()
@@ -58,8 +96,7 @@ class MenuHelper extends AkActionViewHelper
         foreach ($controller_file_names as $controller_file_name=>$options){
             $controller_file_name = array_pop($options);
             $controller_name = str_replace('.php','',$controller_file_name);
-            
-            if(file_exists(AK_CONTROLLERS_DIR.DS.$controller_file_name)){
+            if(strstr($controller_file_name,'_controller.php') && file_exists(AK_CONTROLLERS_DIR.DS.$controller_file_name)){
                 include_once(AK_CONTROLLERS_DIR.DS.$controller_file_name);
                 $controller_class_name = AkInflector::classify($controller_name);
                 $menu_options[str_replace('_controller','',$controller_name)] = $this->_get_this_class_methods($controller_class_name);
@@ -83,7 +120,6 @@ class MenuHelper extends AkActionViewHelper
         $array3 = array_diff($array3, array(strtolower($class), 'index', 'destroy', 'edit', 'show'));
         return($array3);
     }
-
 }
 
 ?>
--- test/unit/lib/AkActionView/helpers/menu_helper.php
+++ test/unit/lib/AkActionView/helpers/menu_helper.php
@@ -2,20 +2,75 @@
 
 require_once('_HelpersUnitTester.php');
 require_once(AK_LIB_DIR.DS.'AkActionView'.DS.'helpers'.DS.'menu_helper.php');
+require_once(AK_LIB_DIR.DS.'AkActionController.php');
+require_once(AK_LIB_DIR.DS.'AkRequest.php');
+require_once(AK_CONTROLLERS_DIR.DS.'..'.DS.'application_controller.php');
 
+Mock::generate('AkRequest');
 
 class MenuHelperTests extends HelpersUnitTester 
 {
-    /**
-     * @todo Add tests for menu helper
-     */
-    function tests_pending()
+    function setUp()
     {
+        $this->controller = &new AkActionController();
+        $this->controller->Request =& new MockAkRequest($this);
+        $this->controller->controller_name = 'test';
+        $this->controller->instantiateHelpers();
         
+        $this->menu_helper =& $this->controller->menu_helper;
     }
+    function tests_menu_for_controllers()
+    {
+        $this->assertEqual(
+            $this->menu_helper->menu_for_controllers(),
+            file_get_contents(AK_TEST_HELPERS_DIR.DS.'menu_helper_all.txt')
+        );
+/*
+        $this->assertEqual(
+            $this->menu_helper->menu_for_controllers(array('advertiser' => array('buy', 'partial_in_template'), 'locale_detection' => 'session', 'page' => 'setup')),
+            file_get_contents(AK_TEST_HELPERS_DIR.DS.'menu_helper_limit.txt')
+        );
+
+        $this->assertEqual(
+            $this->menu_helper->menu_for_controllers(array('unavailable' => array('foo', 'bar'))),
+            '<div id="menu"></div>'
+        );
+
+        $this->assertEqual(
+            $this->menu_helper->menu_for_controllers(array('unavailable' => array('foo', 'bar')), "custom_menu_id"),
+            '<div id="custom_menu_id"></div>'
+        );
+
+        $this->assertEqual(
+            $this->menu_helper->menu_for_controllers(array('advertiser' => array('buy', 'partial_in_template'), 'locale_detection' => 'session', 'page' => 'setup'), 'menu', 'current', ''),
+            file_get_contents(AK_TEST_HELPERS_DIR.DS.'menu_helper_limit_no_title_tag.txt')
+        );
+
+        $this->assertEqual(
+            $this->menu_helper->menu_for_controllers(array('advertiser' => array('buy', 'partial_in_template'), 'locale_detection' => 'session', 'page' => 'setup'), 'menu', 'current', 'p'),
+            file_get_contents(AK_TEST_HELPERS_DIR.DS.'menu_helper_limit_title_tag_p.txt')
+        );
+
+        $this->controller->controller_name = 'Advertiser';
+
+        $this->assertEqual(
+            $this->menu_helper->menu_for_controllers(array('advertiser' => array('buy', 'partial_in_template'), 'locale_detection' => 'session', 'page' => 'setup')),
+            file_get_contents(AK_TEST_HELPERS_DIR.DS.'menu_helper_limit_current.txt')
+        );
+
+        $this->assertEqual(
+            $this->menu_helper->menu_for_controllers(array('advertiser' => array('buy', 'partial_in_template'), 'locale_detection' => 'session', 'page' => 'setup'), 'menu', 'selected'),
+            file_get_contents(AK_TEST_HELPERS_DIR.DS.'menu_helper_limit_current_not_default.txt')
+        );
+
+        $this->assertEqual(
+            $this->menu_helper->menu_for_controllers(array('advertiser' => array('buy', 'partial_in_template'), 'locale_detection' => 'session', 'page' => 'setup'), 'menu', 'selected', ''),
+            file_get_contents(AK_TEST_HELPERS_DIR.DS.'menu_helper_limit_current_not_default_no_title.txt')
+        );
+        */
+    } 
     
 }
-
 
 Ak::test('MenuHelperTests');
 
