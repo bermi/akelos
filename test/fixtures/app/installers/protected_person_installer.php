bd502a5131f81d90b92e5510d7138fa433142cbf
kaste <kaste@a2fa5c27-f921-0410-a72c-bf682d381be0>
2008-04-04 18:29:50 +0000

    Fixes unit tests. active_record_helper.php line 71 & 79 failed with SQLite.
    git-svn-id: http://svn.akelos.org/trunk@532 a2fa5c27-f921-0410-a72c-bf682d381be0

--- test/fixtures/app/installers/protected_person_installer.php
+++ test/fixtures/app/installers/protected_person_installer.php
@@ -5,11 +5,11 @@ class ProtectedPersonInstaller extends AkInstaller
     function up_1()
     {
         $this->createTable('protected_people','
-          id int auto increment not null primary,
+          id,
           name string(32) not null,
           birthday datetime,
           is_active boolean not null default 1,
-			 credit_points int default 1000,
+          credit_points int default 1000,
           created_by int default null,
           created_at,
           updated_at
@@ -18,7 +18,7 @@ class ProtectedPersonInstaller extends AkInstaller
 
     function down_1()
     {
-        $this->dropTable('protected_people');
+        $this->dropTable('protected_people', array('sequence'=>true));
     }
 }
 
