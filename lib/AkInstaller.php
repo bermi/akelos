0b136d0df0ac71b3cfeebb35f127784e5d151d4e
bermiferrer <bermiferrer@a2fa5c27-f921-0410-a72c-bf682d381be0>
2008-04-06 00:55:53 +0000

    Adding generic command line argument promt system.
    This has been extracted from the admin plugin installer so it can be used in other installers. We might also move it to a generic method so it can be used to dynamically request params on non-installer scripts.
    
    
    git-svn-id: http://svn.akelos.org/trunk@541 a2fa5c27-f921-0410-a72c-bf682d381be0

--- lib/AkInstaller.php
+++ lib/AkInstaller.php
@@ -596,6 +596,31 @@ class AkInstaller
         return $this->db->hasTransactionFailed();
     }
 
+    /**
+     * Promts for a variable on console scripts
+     */
+    function promtUserVar($message, $options = array())
+    {
+        $f = fopen("php://stdin","r");
+        $default_options = array(
+        'default' => null,
+        'optional' => false,
+        );
+
+        $options = array_merge($default_options, $options);
+
+        echo "\n".$message.(empty($options['default'])?'': ' ['.$options['default'].']').': ';
+        $user_input = fgets($f, 25600);
+        $value = trim($user_input,"\n\r\t ");
+        $value = empty($value) ? $options['default'] : $value;
+        if(empty($value) && empty($options['optional'])){
+            echo "\n\nThis setting is not optional.";
+            fclose($f);
+            return $this->promtUserVar($message, $options);
+        }
+        fclose($f);
+        return empty($value) ? $options['default'] : $value;
+    }
 
     function _loadDbDesignerDbSchema()
     {
