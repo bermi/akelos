2622c6c374751754d4e25043c31937993e156243
bermi <bermi@a2fa5c27-f921-0410-a72c-bf682d381be0>
2009-05-18 20:03:04 +0000

    Adding email messages to log/mail.log when the Mailer delivery is test and runs on Development mode
    git-svn-id: http://svn.akelos.org/trunk@1304 a2fa5c27-f921-0410-a72c-bf682d381be0

--- lib/AkActionMailer/AkMailDelivery/AkTestDelivery.php
+++ lib/AkActionMailer/AkMailDelivery/AkTestDelivery.php
@@ -1,11 +1,18 @@
 <?php
 
-
 class AkTestDelivery extends AkObject
 {
     function deliver(&$Mailer, $settings = array())
     {
-        $settings['ActionMailer']->deliveries[] = $Mailer->Message->getEncoded();
+        $encoded_message = $Mailer->Message->getEncoded();
+        $settings['ActionMailer']->deliveries[] = $encoded_message;
+        if(AK_DEV_MODE){
+            $Logger = Ak::getLogger();
+            $original_error_file = $Logger->error_file;
+            $Logger->error_file = AK_LOG_DIR.DS.'mail.log';
+            $Logger->message($encoded_message);
+            $Logger->error_file = $original_error_file;
+        }
     }
 }
 
