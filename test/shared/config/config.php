072d810520e33fcb39ace4fda21de0e7ebf38a2e
Bermi Ferrer <bermi@bermilabs.com>
2009-12-12 16:32:16 +0100

    Removing old in-fixtures app dir

--- test/shared/config/config.php
+++ test/shared/config/config.php
@@ -4,9 +4,6 @@ defined('AK_CACHE_HANDLER')         || define('AK_CACHE_HANDLER', 1);
 defined('AK_ENVIRONMENT')           || define('AK_ENVIRONMENT', 'testing');
 defined('AK_TEST_DIR')              || define('AK_TEST_DIR', str_replace(DIRECTORY_SEPARATOR.'shared'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php','',__FILE__));
 defined('AK_FIXTURES_DIR')          || define('AK_FIXTURES_DIR', AK_TEST_DIR.DIRECTORY_SEPARATOR.'fixtures');
-defined('AK_APP_DIR')               || define('AK_APP_DIR', AK_FIXTURES_DIR.DIRECTORY_SEPARATOR.'app');
-defined('AK_PUBLIC_DIR')            || define('AK_PUBLIC_DIR', AK_FIXTURES_DIR.DIRECTORY_SEPARATOR.'public');
-defined('AK_TEST_HELPERS_DIR')      || define('AK_TEST_HELPERS_DIR', AK_FIXTURES_DIR.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'helpers');
 
 if(isset($_SERVER['REQUEST_URI'])){
     defined('AK_SITE_URL_SUFFIX')   || define('AK_SITE_URL_SUFFIX', str_replace(array(join(DIRECTORY_SEPARATOR,array_diff((array)@explode(DIRECTORY_SEPARATOR,AK_TEST_DIR), (array)@explode('/',$_SERVER['REQUEST_URI']))),DIRECTORY_SEPARATOR),array('','/'),AK_TEST_DIR));
