79396e86d57b419a97c479d6219ba4d5c71b61e5
Bermi Ferrer <bermi@bermilabs.com>
2009-12-15 04:05:26 +0100

    Allowing setting authorized ips

--- lib/tasks/doc/website_files/app/controllers/docs_controller.php
+++ lib/tasks/doc/website_files/app/controllers/docs_controller.php
@@ -5,7 +5,7 @@ class DocsController extends ApplicationController
     private $_authorized_users = array('akelos' => 'docs');
     
     public function __construct(){
-        if(!in_array(AK_REMOTE_IP, array('localhost','127.0.0.1','::1'))){
+        if(!in_array(AK_REMOTE_IP, AkConfig::getOption('developer_ips', array('localhost','127.0.0.1','::1')))){
             parent::init();
             $this->beforeFilter(array('authenticate' => array('except' => array('index'))));
         }
