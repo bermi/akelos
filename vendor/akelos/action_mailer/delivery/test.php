69ccf72443039758c91ab5007fee202117167104
Bermi Ferrer <bermi@bermilabs.com>
2010-01-03 21:03:35 +0100

    When using test delivery you can now get the last delivered raw message by calling:
    Ak::getStaticVar('last_mail_delivered')
    
    The mail logger will also log messages on dev mode if we are using the test delivery method

--- vendor/akelos/action_mailer/delivery/test.php
+++ vendor/akelos/action_mailer/delivery/test.php
@@ -5,9 +5,12 @@ class AkTestDelivery
     public function deliver(&$Mailer, $settings = array()) {
         $encoded_message = $Mailer->getRawMessage();
         $settings['ActionMailer']->deliveries[] = $encoded_message;
-        if(AK_DEV_MODE){
+        if(!AK_PRODUCTION_MODE){
             $Logger = Ak::getLogger('mail');
             $Logger->message($encoded_message);
+        }
+        if(AK_TEST_MODE){
+            Ak::setStaticVar('last_mail_delivered', $encoded_message);
         }
     }
 }
