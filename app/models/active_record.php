aed56e59a7744f64e6681c6ee24ab9af1a64c6c4
Bermi Ferrer <bermi@bermilabs.com>
2010-01-08 03:01:01 +0100

    Updating docs

--- app/models/active_record.php
+++ app/models/active_record.php
@@ -1,14 +1,11 @@
 <?php
 
 /**
-* This file is application-wide model file. You can put all
+* This file is application-wide active record model file. You can put all
 * application-wide model-related methods here.
 *
 * Add your application-wide methods in the class below, your models
 * will inherit them.
-*
-* @package ActiveRecord
-* @subpackage Base
 */
 class ActiveRecord extends BaseActiveRecord
 {
