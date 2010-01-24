5ace8e4b5199be61a3b1218d88e263177754abca
Bermi Ferrer <bermi@bermilabs.com>
2010-01-24 16:06:38 +0100

    Adding digest http authentication (untested)

--- action_pack/controller_authentication.php
+++ action_pack/controller_authentication.php
@@ -12,7 +12,7 @@
 *
 *   class PostsController extends ApplicationController
 *   {
-*       public $_authorized_users = array('bermi' => 'secret');
+*       private $_authorized_users = array('bermi' => 'secret');
 *
 *       public function __construct(){
 *           $this->beforeFilter(array('authenticate' => array('except' => array('index'))));
@@ -90,6 +90,21 @@ class AkControllerAuthentication
         return $this->_authenticationRequest($realm);
     }
 
+    public function authenticateOrRequestWithHttpDigest($realm = AK_APP_NAME, $login_procedure) {
+        if($Result = $this->authenticateWithHttpDigest($realm, $login_procedure)){
+            return $Result;
+        }
+        return $this->requestHttpDigestAuthentication($realm);
+    }
+
+    public function authenticateWithHttpDigest($realm, $login_procedure) {
+        return $this->_authenticateDiggest($realm, $login_procedure);
+    }
+
+    public function requestHttpDigestAuthentication($realm = AK_APP_NAME) {
+        return $this->_authenticationDigestRequest($realm);
+    }
+
     /**
      * This is method takes a $login_procedure for performing access authentication.
      *
@@ -109,7 +124,7 @@ class AkControllerAuthentication
      *
      * @return bool
      */
-    public function _authenticate($login_procedure) {
+    private function _authenticate($login_procedure) {
         if(!$this->_authorization()){
             return false;
         }else{
@@ -118,22 +133,24 @@ class AkControllerAuthentication
                 if(!isset($login_procedure['handler'])){
                     return isset($login_procedure[$user_name]) && $login_procedure[$user_name] == $password;
                 }elseif(is_object($login_procedure['handler']) && method_exists($login_procedure['handler'], $login_procedure['method'])){
-                    return $login_procedure['handler']->$login_procedure['method']($user_name, $password, $this);
+                    return $login_procedure['handler']->$login_procedure['method']($user_name, $password, $this->_Controller);
                 }
             }elseif(method_exists($login_procedure, 'authenticate')){
-                return $login_procedure->authenticate($user_name, $password, $this);
+                return $login_procedure->authenticate($user_name, $password, $this->_Controller);
             }
         }
         return false;
     }
 
-    public function _getUserNameAndPassword() {
+    private function _getUserNameAndPassword() {
         $credentials = $this->_decodeCredentials();
         return !is_array($credentials) ? explode(':', $credentials , 2) : $credentials;
     }
 
-    public function _authorization() {
+    private function _authorization() {
+
         return
+        empty($this->_Controller->Request->env['PHP_AUTH_DIGEST']) ? (
         empty($this->_Controller->Request->env['PHP_AUTH_USER']) ? (
         empty($this->_Controller->Request->env['HTTP_AUTHORIZATION']) ? (
         empty($this->_Controller->Request->env['X-HTTP_AUTHORIZATION']) ? (
@@ -143,10 +160,11 @@ class AkControllerAuthentication
         ) : $this->_Controller->Request->env['X_HTTP_AUTHORIZATION']
         ) : $this->_Controller->Request->env['X-HTTP_AUTHORIZATION']
         ) : $this->_Controller->Request->env['HTTP_AUTHORIZATION']
-        ) : array($this->_Controller->Request->env['PHP_AUTH_USER'], $this->_Controller->Request->env['PHP_AUTH_PW']);
+        ) : array($this->_Controller->Request->env['PHP_AUTH_USER'], $this->_Controller->Request->env['PHP_AUTH_PW'])) :
+        $this->_parseHttpDiggest($this->_Controller->Request->env['PHP_AUTH_DIGEST']);
     }
 
-    public function _decodeCredentials() {
+    private function _decodeCredentials() {
         $authorization = $this->_authorization();
         if(is_array($authorization)){
             return $authorization;
@@ -155,11 +173,11 @@ class AkControllerAuthentication
         return base64_decode(array_pop($credentials));
     }
 
-    public function _encodeCredentials($user_name, $password) {
+    private function _encodeCredentials($user_name, $password) {
         return 'Basic '.base64_encode("$user_name:$password");
     }
 
-    public function _authenticationRequest($realm) {
+    private function _authenticationRequest($realm) {
         header('WWW-Authenticate: Basic realm="' . str_replace('"','',$realm) . '"');
 
         if(method_exists($this, 'access_denied')){
@@ -169,6 +187,49 @@ class AkControllerAuthentication
             echo "HTTP Basic: Access denied.\n";
             exit;
         }
+    }
+
+    private function _authenticateDiggest($realm, $login_procedure){
+        if(!$data = $this->_authorization()){
+            return false;
+        }else{
+            $user_name = $data['username'];
+            if(is_array($login_procedure)){
+                if(!isset($login_procedure['handler'])){
+                    $password = isset($login_procedure[$user_name]) ? $login_procedure[$user_name] : false;
+                }elseif(is_object($login_procedure['handler']) && method_exists($login_procedure['handler'], $login_procedure['method'])){
+                    $password = $login_procedure['handler']->$login_procedure['method']($user_name, $this->_Controller);
+                }
+                if($password){
+                    return $data['response'] == md5(
+                    md5($user_name.':'.$realm.':'.$password).
+                    ':'.$data['nonce'].':'.$data['nc'].':'.$data['cnonce'].':'.$data['qop'].':'.
+                    md5($this->_Controller->Request->env['REQUEST_METHOD'].':'.$data['uri']));
+                }
+            }
+
+        }
+        return false;
+    }
+
+    private function _authenticationDigestRequest($realm) {
+        header('WWW-Authenticate: Digest realm="'.str_replace('"','',$realm).'",qop="auth",nonce="'.uniqid().'",opaque="'.md5($realm).'"');
+        if(method_exists($this, 'access_denied')){
+            $this->_Controller->access_denied();
+        }else{
+            header('HTTP/1.0 401 Unauthorized');
+            echo "HTTP Basic: Access denied.\n";
+            exit;
+        }
+    }
+
+    private function _parseHttpDiggest($digest){
+        $matches = array();
+        $data = array();
+        if(preg_match_all('@(username|nonce|uri|nc|cnonce|qop|response)=[\'"]?([^\'",]+)@', $digest, $matches)){
+            $data = array_combine($matches[1], $matches[2]);
+        }
+        return (count($data)==7) ? $data : false;
     }
 
     public function setExtendedBy(&$Controller) {
