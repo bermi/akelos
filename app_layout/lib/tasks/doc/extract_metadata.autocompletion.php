2aede3ed378fc4767b72038e01cec3fb04ae588e
Bermi Ferrer <bermi@bermilabs.com>
2010-01-24 23:39:35 +0100

    Removing unnecessary files

--- app_layout/lib/tasks/doc/extract_metadata.autocompletion.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$suggestions = array(); 
-
-// each line is a tab option if bash autocompletion is enabled
-echo join("\n", $suggestions);
--- app_layout/lib/tasks/doc/makefile.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-makelos_task('doc:extract_metadata', array(
-    'description' => 'Describe your task'
-));
-
-
