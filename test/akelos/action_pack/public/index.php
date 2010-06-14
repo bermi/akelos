c6503760c3a784393b6923ba3713996513c5b5ea
Bermi Ferrer <bermi@bermilabs.com>
2010-06-14 03:34:36 +0200

    Updating config for action pack tests

--- test/akelos/action_pack/public/index.php
+++ test/akelos/action_pack/public/index.php
@@ -1,5 +1,12 @@
 <?php
 
+// Core tests need to run without installing a new app
+$_config_path_candidate = realpath(dirname(__FILE__).'/../../../../app_layout/config');
+if($_config_path_candidate){
+    define('AK_CONFIG_DIR', $_config_path_candidate);
+}
+unset($_config_path_candidate);
+
 define('AK_ENABLE_URL_REWRITE',     false);
 define('AK_URL_REWRITE_ENABLED',    false);
 
--- test/akelos/config.php
+++ test/akelos/config.php
@@ -3,6 +3,8 @@
 require_once(dirname(__FILE__).'/../shared/config/config.php');
 
 AkConfig::setOption('testing_url', 'http://akelos.tests/akelos');
+AkConfig::setOption('action_controller.session', array("key" => "_myapp_session", "secret" => "c1ef4792-42c5-b484-819e-16750c71cddb"));
+
 AkUnitTestSuite::checkIfTestingWebserverIsAccesible(array('base_path' => dirname(__FILE__)));
 AkConfig::setOption('memcached_enabled', AkMemcache::isServerUp());
 
