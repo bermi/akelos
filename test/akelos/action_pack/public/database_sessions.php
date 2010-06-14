159b73cedd15ee7ea7d9a22e800de454508f4d49
Bermi Ferrer <bermi@bermilabs.com>
2010-06-14 04:36:57 +0200

    Fixing paths for db session tests

--- test/akelos/action_pack/public/database_sessions.php
+++ test/akelos/action_pack/public/database_sessions.php
@@ -1,5 +1,19 @@
 <?php
 
+// Core tests need to run without installing a new app
+$_config_path_candidate = realpath(dirname(__FILE__).'/../../../../app_layout/config');
+if($_config_path_candidate){
+    define('AK_CONFIG_DIR', $_config_path_candidate);
+}
+unset($_config_path_candidate);
+
+define('AK_ENABLE_URL_REWRITE',     false);
+define('AK_URL_REWRITE_ENABLED',    false);
+
+if(isset($_GET['custom_routes'])){
+    define('AK_ROUTES_MAPPING_FILE', dirname(__FILE__).'/../'.str_replace('.','',$_GET['custom_routes']).'_routes.php');
+}
+
 define ('AK_SESSION_HANDLER', 1);
 
 if(isset($_GET['expire'])){
@@ -7,6 +21,7 @@ if(isset($_GET['expire'])){
 }
 
 require_once(dirname(__FILE__).'/../config.php');
+
 
 Ak::db();
 AkDbSession::install();
